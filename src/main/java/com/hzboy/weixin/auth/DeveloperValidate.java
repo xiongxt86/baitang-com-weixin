07a8a5371a66c4b41e11741a1ce83db8ab933f2b
xiongxiaotun <xiongxiaotun@PC-20150415ECYJ>
2015-04-21 18:16:43 +0800

    一直pombug

--- src/main/java/com/hzboy/weixin/auth/DeveloperValidate.java
+++ src/main/java/com/hzboy/weixin/auth/DeveloperValidate.java
@@ -1,9 +1,12 @@
 package com.hzboy.weixin.auth;
 /**
- * 开发者验证
- * @author Administrator
+ * 
+ * @ClassName: DeveloperValidate 
+ * @Description: TODO(验证开发者) 
+ * @author xiongxiaotun@126.com 
+ * @date 2015年4月21日 下午5:31:18 
  *
  */
 public class DeveloperValidate {
-
+	
 }
