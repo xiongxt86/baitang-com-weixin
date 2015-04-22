774c68f3448fb0af00201f40f76f938c6cf0d1aa
xiongxt <xiongxiaotun@126.com>
2015-04-23 02:14:02 +0800

    1

--- src/main/java/com/hzboy/weixin/Test.java
+++ src/main/java/com/hzboy/weixin/Test.java
@@ -1,12 +1,9 @@
 package com.hzboy.weixin;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.entity.mime.MultipartEntity;
 
 public class Test{
 	
 	public static void main(String[] args) {
-		MultipartEntity mpEntity = new MultipartEntity();
 	}
 	
 }
