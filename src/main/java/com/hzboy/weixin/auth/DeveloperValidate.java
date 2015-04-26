3d24a6cc1f2cd7de8ca0cb54e75f9e08810cf916
xiongxt <xiongxiaotun@126.com>
2015-04-26 18:04:31 +0800

    验证url和token

--- src/main/java/com/hzboy/weixin/auth/DeveloperValidate.java
+++ src/main/java/com/hzboy/weixin/auth/DeveloperValidate.java
@@ -1,9 +1,14 @@
 package com.hzboy.weixin.auth;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import core.SignUtil;
 
 /**
  * 
@@ -14,12 +19,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
  *
  */
 @Controller
-@RequestMapping("develop")
 public class DeveloperValidate {
 	
 	@RequestMapping("auth")
-	@ResponseBody
-	public String auth(Model model){
-		return "success";
+	public void auth(HttpServletResponse response, String signature,String timestamp,String nonce,String echostr){
+		 PrintWriter out;
+		try {
+			out = response.getWriter();
+			// 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败  
+			if (SignUtil.checkSignature(signature, timestamp, nonce)) {  
+				out.print(echostr);  
+			}  
+			out.close();  
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
 	}
 }
